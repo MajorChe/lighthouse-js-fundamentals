246f77e6861edd29571d30623b19143d1add77e9
charit nanduri <charithnanduri03@gmail.com>
2021-10-06 02:11:52 +0000

    New files added including while

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chorus ="Lets make the country proud!";
+let repeat =0;
+while(repeat<=10){
+  if(repeat===5){
+    console.log("*change the key*")
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Lets start the song");
