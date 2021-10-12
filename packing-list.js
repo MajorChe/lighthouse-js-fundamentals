35ed757d8889a4d7aafa5229a24aa3625b3b3ab1
charit nanduri <charithnanduri03@gmail.com>
2021-10-12 21:46:11 +0000

    while loop added in packing list

--- packing-list.js
+++ packing-list.js
@@ -1,5 +1,7 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 console.log("Kitchen item to pack are: ");
-for(let i = 0; i<packingList.length; i++){
+let i = 0;
+while(i < packingList.length){
   console.log(packingList[i]);
+  i++;
 }
