e58c23fba53c1a16430ad6293c6f5e9fae89f877
charit nanduri <charithnanduri03@gmail.com>
2021-10-12 21:44:02 +0000

    packing list added

---
+++ packing-list.js
@@ -0,0 +1,5 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+console.log("Kitchen item to pack are: ");
+for(let i = 0; i<packingList.length; i++){
+  console.log(packingList[i]);
+}
